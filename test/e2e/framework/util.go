a0c3bcbe2b4d775d75acf6ef9111941780e2572a
Claudiu Belu <cbelu@cloudbasesolutions.com>
2018-06-19 05:40:28 -0700

    Removes HostNetwork for hostexec pods
    HostNetwork=true is not supported by Windows (pods fail to spawn), and
    it isn't needed to pass the tests.
    
    Makes the hostexec pod commands more Windows friendly.

--- test/e2e/framework/util.go
+++ test/e2e/framework/util.go
@@ -471,7 +471,7 @@ func ProxyMode(f *Framework) (string, error) {
 			Namespace: f.Namespace.Name,
 		},
 		Spec: v1.PodSpec{
-			HostNetwork: true,
+			//HostNetwork: true,
 			Containers: []v1.Container{
 				{
 					Name:    "detector",
@@ -3364,7 +3364,7 @@ func NewHostExecPodSpec(ns, name string) *v1.Pod {
 					ImagePullPolicy: v1.PullIfNotPresent,
 				},
 			},
-			HostNetwork:                   true,
+			//HostNetwork:                   true,
 			SecurityContext:               &v1.PodSecurityContext{},
 			TerminationGracePeriodSeconds: &immediate,
 		},
