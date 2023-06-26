a19e266864f0f1a80e26e24663cc766dcc48b515
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2023-06-26 17:53:13 +0300

    windows-tests: Add retries to Windows `assertConsistentConnectivity` func
    Add retry logic to the `assertConsistentConnectivity` function from
    the `test/e2e/windows/hybrid_network.go` file.
    
    Signed-off-by: Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
    (cherry picked from PR https://github.com/kubernetes/kubernetes/pull/120254)

--- test/e2e/windows/service.go
+++ test/e2e/windows/service.go
@@ -82,7 +82,7 @@ var _ = SIGDescribe("Services", func() {
 		framework.ExpectEqual(testPod.Spec.NodeSelector["kubernetes.io/os"], "windows")
 
 		ginkgo.By(fmt.Sprintf("checking connectivity Pod to curl http://%s:%d", nodeIP, nodePort))
-		assertConsistentConnectivity(ctx, f, testPod.ObjectMeta.Name, windowsOS, windowsCheck(fmt.Sprintf("http://%s", net.JoinHostPort(nodeIP, strconv.Itoa(nodePort)))))
+		assertConsistentConnectivity(ctx, f, testPod.ObjectMeta.Name, windowsOS, windowsCheck(fmt.Sprintf("http://%s", net.JoinHostPort(nodeIP, strconv.Itoa(nodePort)))), internalMaxTries)
 
 	})
 
